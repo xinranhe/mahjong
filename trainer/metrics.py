2dd6fb925056f84c604b142e415e6ecbc910212a
Xinran He <xhe2@snapchat.com>
2019-03-18 14:46:23 -0700

    fix metrics computation

--- trainer/metrics.py
+++ trainer/metrics.py
@@ -4,4 +4,5 @@ def compute_accuracy(labels, logits):
     prediction = tf.argmax(logits, axis=1)
     max_mask = tf.one_hot(prediction, 14)
     is_acc = tf.cast(max_mask * labels > 0, dtype=tf.float32)
+    is_acc = tf.reduce_sum(is_acc, axis=1)
     return is_acc
